aa4f7a451312d7842c6983243c44d703b337a783
UZAIR SHAH <137003543+uzishah@users.noreply.github.com>
2023-11-25 16:12:22 +0500

    Add files via upload

---
+++ array/app.js
@@ -0,0 +1,144 @@
+// qno 1
+// var studentName = []
+
+
+
+// q no2
+
+// var newArray = new Array()
+
+// qno3
+
+// var fruit = ["apple", "mango","banana","orange"]
+// console.log(fruit)
+
+// q no4
+
+// var numberArray = [1,2,3,4,5]
+// console.log(numberArray)
+
+// qno5
+
+// var newArray = [true, false, true, false]
+// console.log(newArray[0])
+
+
+//qno 6
+
+// var newArray = [2,false, "shah", "uzair",4]
+// console.log(newArray[2])
+
+// qno7
+
+// var qualification = ["SSC","FSC","HSC","BA","BCOM"]
+// document.write(qualification)
+
+
+
+// qno8
+
+
+
+// var student = ["uzair", "saif","syed"]
+// var marks= [300,250,400]
+// totalMarks = 500
+// persntage1 = marks[0]*100/totalMarks
+// persntage2 = marks[1]*100/totalMarks
+// persntage3 = marks[2]*100/totalMarks
+
+// console.log("score of"+student[0]+"is"+marks[0]+", and persentage is:"+persntage1)
+
+// console.log("score of"+student[1]+"is"+marks[1]+", and persentage is:"+persntage2)
+// console.log("score of"+student[2]+"is"+marks[2]+", and persentage is:"+persntage3)
+
+
+
+// qno9
+
+// var colour =['white','black','orange','pink']
+// console.log(colour)
+// colour.unshift(prompt())
+// console.log(colour)
+
+// console.log(colour)
+// colour.push(prompt())
+// console.log(colour)
+
+
+
+// colour.unshift(prompt())
+//  colour.unshift(prompt())
+// console.log(colour)
+
+
+//  colour.shift()
+//  console.log(colour)
+
+
+//  colour.pop()
+// console.log(colour)
+
+
+// colour.splice(prompt(),prompt(),prompt())
+
+// console.log(colour)
+
+
+// slicedClour = colour.slice(prompt(),prompt())
+// console.log(slicedClour)
+
+
+
+
+// var cities = ['karachi','islamabad','quetta','peshawer','badeen']
+// console.log(cities)
+// selectedCities = cities.slice(prompt(),prompt())
+// console.log(selectedCities)
+
+
+
+// var newArray = ['this','is','my','cat']
+// result = newArray.join(' ')
+// console.log(result)
+
+
+
+//  var cities = []
+//  cities.push('karachi')
+//  cities.push('islamabad')
+//  cities.push('quetta')
+//  cities.push('peshawer')
+//  cities.push('badeen')
+//  console.log(cities)
+ 
+
+//  var firstcity= cities.shift()
+//  console.log(firstcity)
+//  var secondCity = cities.shift()
+//  var thirdCity = cities.shift()
+//  var forthCity = cities.shift()
+// console.log(firstcity)
+// console.log(secondCity)
+// console.log(thirdCity)
+// console.log(forthCity)
+
+
+
+
+ var cities = []
+ cities.push('karachi')
+ cities.push('islamabad')
+ cities.push('quetta')
+ cities.push('peshawer')
+ cities.push('badeen')
+ console.log(cities)
+ 
+
+ var firstcity= cities.pop()
+ console.log(firstcity)
+ var secondCity = cities.pop()
+ var thirdCity = cities.pop()
+ var forthCity = cities.pop()
+console.log(secondCity)
+console.log(thirdCity)
+console.log(forthCity)
